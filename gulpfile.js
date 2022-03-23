16e7e9fc2b8da2fc9476a6a97db8bf1303518d24
kanav-raina <kanavraina98@gmail.com>
2022-03-23 13:27:31 +0530

    basic setup

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const { watch, series, parallel, src, dest, gulp } = require('gulp');
+const connect = require('gulp-connect'); // Runs a local webserver
+const open = require('gulp-open'); // Opens a URL in a web browser
+
+// Launch Chrome web browser
+// https://www.npmjs.com/package/gulp-open
+function openBrowser(done) {
+    var options = {
+    uri: 'http://localhost:8082'
+    };
+    return src('./')
+    .pipe(open(options));
+    done();
+}
+
+// Gulp plugin to run a webserver (with LiveReload)
+// https://www.npmjs.com/package/gulp-connect
+function server(done) {
+    return connect.server({
+    root: './',
+    port: 8082,
+    debug: true,
+    });
+    done();
+}
+
+// Default Gulp command
+exports.default = series(openBrowser, server);
